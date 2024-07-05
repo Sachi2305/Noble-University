35b00d585603c1ba03bbfe02b97df8421dbf4e4a
Sachi2305 <sachipatel2305@gmail.com>
2024-07-05 13:21:34 +0530

    My react project

---
+++ src/Components/About/About.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './About.css'
+import about_img from '../../assets/about.png'
+import play_icon from '../../assets/play-icon.png'
+
+const About = () => {
+  return (
+    <div className='about'>
+      <div className='about-left'>
+       <img src={about_img} alt='' className='about-img' /> 
+       <img src={play_icon} alt='' className='play-icon' />
+      </div>
+      <div className='about-right'> </div>
+      <h3>About Univercity</h3>
+      <h2>Nurturing Tomorrow's Leaders Today</h2>
+      <p>Embark on a transformative educational journey with our university comprehensive education programs. Our cutting-edge curriculum is designed to empower students with the knoledge, skils, and experiences needed to excel in the dynamic fied of education.</p>
+      <p>With a focus on innovation, hands-on earning, and personalized mentorship, our programs prepare aspiring educators to make a meaningful impact in schools and communities.</p>
+      <p>Embark on a transformative educational journey with our university comprehensive education programs. Our cutting-edge curriculum is designed to empower students with the knoledge, skils, and experiences needed to excel in the dynamic fied of education.</p>
+    </div>
+  )
+}
+
+export default About
