cb794d4d136d5b3042af7c6c5c073ee6a18e045e
Himil192 <himilprajapati2611@gmail.com>
2024-07-12 23:56:56 +0530

    change photo

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './About.css'
-import about_img from '../../assets/about.png'
+import about_img from '../../assets/about.jpg'
 import play_icon from '../../assets/play-icon.png'
 
 const About = ({setPlayState}) => {
